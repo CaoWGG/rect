38f657c26a24c5f2e080ee0aecba40e87a991c2e
CaoWGG <cwgnlfd@163.com>
2018-09-01 22:18:51 +0800

    矩形框融合

--- rect_rh.py
+++ rect_rh.py
@@ -1,4 +1,28 @@
 import numpy as np
+import cv2
+
+box=np.array(
+ [[1225 , 184 ,1246  ,240],
+ [1226 , 187 ,1250  ,238],
+ [1268 , 186 ,1297 , 241],
+ [1269 , 188 ,1297 , 239],
+ [1021 , 538 ,1038 ,620],
+ [1024 , 542 ,1043 , 619],
+ [1499 , 459 ,1528 , 526],
+ [1645 , 444 ,1662 , 530],
+ [1584 , 455 ,1610 , 532],
+ [1652 , 465 ,1676 , 529],
+ [1502 , 461 ,1530 , 526],
+ [1588 , 468 ,1619 , 531],
+ [1456 , 435 ,1474 , 534],
+ [1505 , 460 ,1527 , 528],
+ [1654 , 470 ,1674 , 531],
+ [1590 , 469 ,1621 , 532],
+ [1024 , 547 ,1045 , 617],
+ [1504 , 463 ,1529 , 524],
+ [1504  ,468 ,1527 , 527],
+ [1506 , 472 ,1528 , 525]])
+
 def group_rect(box,iou):
     keep_ = []
     if len(box)>1:
@@ -14,8 +38,8 @@ def group_rect(box,iou):
             yy1=np.maximum(y1[i],y1[order[1:]])
             xx2=np.minimum(x2[i],x2[order[1:]])
             yy2=np.minimum(y2[i],y2[order[1:]])
-            w=np.maximum(0.,xx2-xx1+1)
-            h=np.maximum(0.,yy2-yy1+1)
+            w=np.maximum(0.,xx2-xx1)
+            h=np.maximum(0.,yy2-yy1)
             inter=w*h
             ovr=inter/(area[i]+area[order[1:]]-inter)
             inds=np.where(ovr>iou)+np.array([1])
@@ -38,51 +62,23 @@ def group_rect(box,iou):
             keep_.append(list([x1[0], y1[0], x2[0], y2[0]]))
     return keep_
 
-def find_rect(rect,point,pick):
-    points=[]
-    for p in point:
-        x,y=int((p[0]+p[2])/2),((p[1]+p[3])/2)
-        for i in rect:
-            if i[0]<x and x<(i[0]+i[2]):
-                if i[1]<y and y<(i[1]+i[3]):
-                    points.append([p[0]-i[0]+pick[i[4]][0],
-                                   p[1]-i[1]+pick[i[4]][1],
-                                   p[2]-i[0]+pick[i[4]][0],
-                                   p[3]-i[1]+pick[i[4]][1]])
-    return points
+box=np.array(box)
+box=box+np.array([-1000,-50,-1000,-50])
+keep=group_rect(box,0.2)
 
-def sliding_window(image, stepSize, windowSize):
-    for y in range(0, image.shape[0], stepSize):
-        for x in range(0, image.shape[1], stepSize):
-            if y+stepSize> image.shape[0] and x+stepSize> image.shape[1]:
-                yield (image.shape[1]-windowSize[1],image.shape[0]-windowSize[0],image[image.shape[0]-windowSize[0]:image.shape[0],image.shape[1]-windowSize[1]:image.shape[1]])
-            elif y+stepSize> image.shape[0]:
-                yield (x,image.shape[0]-windowSize[0],image[image.shape[0]-windowSize[0]:image.shape[0], x:x + windowSize[0]])
-            elif x+stepSize> image.shape[1]:
-                yield (image.shape[1]-windowSize[1],y,image[y:y + windowSize[1], image.shape[1]-windowSize[1]:image.shape[1]])
-            else:
-                yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
+draw=np.zeros([600,700,3],np.uint8)
 
+for x in box:
+    pt1,pt2=(x[0],x[1]),(x[2],x[3])
+    cv2.rectangle(draw,pt1,pt2,(255,0,0),1)
+	
+cv2.imshow('',draw)
+cv2.waitKey(0)
+for x in keep:
+    pt1,pt2=(x[0],x[1]),(x[2],x[3])
+    cv2.rectangle(draw,pt1,pt2,(0,0,255),1)
+	
+cv2.imshow('',draw)
+cv2.waitKey(0)
 
-def updata(frame_pick,point):
-    frame_pick[4]=frame_pick[3]
-    frame_pick[3]=frame_pick[2]
-    frame_pick[2]=frame_pick[1]
-    frame_pick[1]=frame_pick[0]
-    frame_pick[0]=point
-    return frame_pick
-
-def expend_rect(pick,frame):
-    w,h=frame.shape[1]-1,frame.shape[0]-1
-    pick=pick+np.array([-20,-20,20,20])
-    pick=np.minimum([w,h,w,h],pick)
-    pick=np.maximum([0, 0, 0, 0], pick)
-    return pick
-
-def get_frame_rect(frame_pick):
-    pick = list(frame_pick[4])
-    pick.extend(frame_pick[3])
-    pick.extend(frame_pick[2])
-    pick.extend(frame_pick[1])
-    pick.extend(frame_pick[0])
-    return pick
+cv2.destroyAllWindows()
